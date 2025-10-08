6896b34744e3e5832335355129721876fce8cc9a
AnnaGabrielle17 <145567496+AnnaGabrielle17@users.noreply.github.com>
2025-10-08 08:17:36 -0300

    programandobola

--- jogopong/Assets/Scripts/ControleDosJogadores.cs
+++ jogopong/Assets/Scripts/ControleDosJogadores.cs
@@ -5,6 +5,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float velocidadeDoJogador;
 
     public bool jogador1;
+
+    public float yMinimo;
+
+    public float yMaximo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,12 +29,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
     private void MoverJogador1()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, yMinimo, yMaximo));
+
+        if(Input.GetKey(KeyCode.W))
         {
            transform.Translate(Vector2.up * velocidadeDoJogador * Time.deltaTime);
         }
 
-        if(Input.GetKeyDown(KeyCode.S))
+        if(Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector2.down * velocidadeDoJogador * Time.deltaTime);
         }
@@ -39,12 +46,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void MoverJogador2()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+          transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, yMinimo, yMaximo));
+
+        if(Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector2.up * velocidadeDoJogador * Time.deltaTime);
         }
 
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if(Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.down * velocidadeDoJogador * Time.deltaTime);
         }
