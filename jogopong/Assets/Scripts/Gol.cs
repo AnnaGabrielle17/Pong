3273101dc72504ae2646c6ed2ed03f1bff96ec3b
AnnaGabrielle17 <145567496+AnnaGabrielle17@users.noreply.github.com>
2025-10-09 11:48:39 -0300

    pontuacaodojogador

---
+++ jogopong/Assets/Scripts/Gol.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Gol : MonoBehaviour
+{
+    public bool golDoJogador1;
+   
+   void OnTriggerEnter2D(Collider2D other)
+   {
+    if(golDoJogador1 == true)
+    {
+
+    }
+    else
+    {
+        
+    }
+   }
+}
